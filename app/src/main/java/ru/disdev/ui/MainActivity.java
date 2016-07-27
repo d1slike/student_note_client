193d0887847e2cd6bcce71e30ff3bc430edc1837
d1slike <eldustru@yandex.ru>
2016-07-27 11:53:58 +0300

    New main activity

---
+++ app/src/main/java/ru/disdev/ui/MainActivity.java
@@ -0,0 +1,128 @@
+package ru.disdev.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import ru.disdev.R;
+import ru.disdev.UserSettings;
+import ru.disdev.controllers.AuthController;
+import ru.disdev.model.UserInfo;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        TextView userName = (TextView) navigationView.getHeaderView(0).findViewById(R.id.userNameText);
+        TextView userEmail = (TextView) navigationView.getHeaderView(0).findViewById(R.id.userEmailText);
+
+        userName.setText(UserSettings.getUserInfo().getName());
+        userEmail.setText(UserSettings.getUserInfo().getEmail());
+
+        Menu menu = navigationView.getMenu();
+        MenuItem elderMenu = menu.findItem(R.id.nav_elder_menu);
+        final boolean needShotElderMenu = UserSettings.needShowElderMenu();
+        elderMenu.setVisible(needShotElderMenu);
+        if (needShotElderMenu) {
+            UserInfo info = UserSettings.getUserInfo();
+            menu.findItem(R.id.nav_group_menu).setVisible(info.isCanEditThisGroup());
+            menu.findItem(R.id.nav_new_post).setVisible(info.isCanMakeNewPost());
+        }
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_change_user:
+                Intent showLogin = new Intent(MainActivity.this, AuthActivity.class);
+                showLogin.putExtra(AuthController.MAKE_LOGOUT_EXTRA_ID, true);
+                showLogin.putExtra(AuthController.SILENT_SIGN_IN_EXTRA_ID, false);
+                startActivity(showLogin);
+                finish();
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
